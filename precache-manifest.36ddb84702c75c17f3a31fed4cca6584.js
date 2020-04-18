845358d8d21364be93c5bedc9febcb20bbf897b7
davidn07 <nirmaldavid96@gmail.com>
2020-04-18 12:34:03 +0530

    Updates

---
+++ precache-manifest.36ddb84702c75c17f3a31fed4cca6584.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e2dd9e3b22fcfa2f04188a6ea07d461",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "d06e9daf20c21dd1530b",
+    "url": "/covid-19/static/css/2.a43b59a1.chunk.css"
+  },
+  {
+    "revision": "731e850294693d83ee49",
+    "url": "/covid-19/static/css/main.37d74136.chunk.css"
+  },
+  {
+    "revision": "d06e9daf20c21dd1530b",
+    "url": "/covid-19/static/js/2.9304805b.chunk.js"
+  },
+  {
+    "revision": "eaaf63558baae456918a991a401208d7",
+    "url": "/covid-19/static/js/2.9304805b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "731e850294693d83ee49",
+    "url": "/covid-19/static/js/main.1560ff25.chunk.js"
+  },
+  {
+    "revision": "b2aaa2dbdbb0f7dfe924",
+    "url": "/covid-19/static/js/runtime-main.e2ad220f.js"
+  }
+]);
